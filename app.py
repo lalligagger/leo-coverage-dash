376f6c2fb1203a26c26d1beae1aabe283f71300a
Kevin Lalli <klalli86@gmail.com>
2022-04-17 14:19:54 -0700

    Update app.py
    app should be on 0.0.0.0 after all?

--- app.py
+++ app.py
@@ -204,5 +204,5 @@ def update_map(continent, maptype, settings):
 
 
 if __name__ == "__main__":
-    # app.run_server(debug=True, host="0.0.0.0")
-    app.run_server(debug=True)
+    app.run_server(debug=True, host="0.0.0.0")
+#     app.run_server(debug=True)
